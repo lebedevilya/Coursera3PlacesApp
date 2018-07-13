5293ab0be2b62d21ed5d87133e904a5b94c46048
lebedevilya <ilyalebedev1@yandex.ru>
2018-07-13 21:42:08 +0300

    query progress

--- places/app/models/place.rb
+++ places/app/models/place.rb
@@ -1,10 +1,12 @@
-class Place
+class Place < ActionController::Base
+  include ActiveModel::Model
+  helper PlacesHelper
   attr_accessor :id, :formatted_address, :location, :address_components
   def self.mongo_client
     Mongoid::Clients.default
   end
 
-  def initialize(params={})
+  def initialize(params)
   	@id = params[:_id].to_s
   	@formatted_address = params[:formatted_address]
   	@location = Point.new(params[:geometry][:location])
@@ -14,12 +16,41 @@ class Place
   	end
   end
 
+  def destroy
+ #   pp Place.collection.find(:_id => BSON::ObjectId.from_string(@id))
+  end
+
   def self.collection
     mongo_client['places']
   end
 
   def self.load_all(f)
   	places=JSON.parse(f.read)
-  	self.collection.insert_many(places)
+  	collection.insert_many(places)
+  end
+
+  def self.find_by_short_name(short_name)
+    collection.find({'address_components.short_name' => short_name})
+  end
+
+  def self.to_places(collection)
+    places = []
+    collection.each do |item|
+      places << Place.new(item)
+    end
+    return places
+  end
+
+  def self.find(id)
+    id = BSON::ObjectId.from_string(id)
+    Place.new(self.collection.find({:_id => id}).first)
+  end
+
+  def self.all(offset = 0, limit = nil)
+    result = collection.find.skip(offset)
+    result = result.limit(limit) if limit
+    ans = []
+    result.each {|p| ans << Place.new(p)}
+    return ans
   end
 end
