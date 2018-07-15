39ebc11860c6f673a8cd06c012264eea6d1b57ed
lebedevilya <ilyalebedev1@yandex.ru>
2018-07-15 20:55:06 +0300

    started geolocation

--- places/app/models/place.rb
+++ places/app/models/place.rb
@@ -81,4 +81,20 @@ class Place < ActionController::Base
     ]
     result = collection.find.aggregate(aggregate_array).to_a.map {|h| h[:_id].to_s}
   end
+
+  def self.create_indexes
+    collection.indexes.create_one({'geometry.geolocation': Mongo::Index::GEO2DSPHERE})
+  end
+
+  def self.remove_indexes
+    collection.indexes.drop_one('geometry.geolocation_2dsphere')
+  end
+
+  def self.near(point, max_meters = nil)
+    request = {:$geometry => point.to_hash}
+    request[:$maxDistance] = max_meters if max_meters
+    collection.find('geometry.geolocation' => {
+      :$near => request
+    })
+  end
 end
